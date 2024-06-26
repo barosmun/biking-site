5331ceef286a80044f7d4d72d11d25f34babca43
Barrett Osmundson <barrettosmundson@gmail.com>
2024-06-26 17:01:06 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import IconBike from '~icons/mdi/bike';
+	// import IconBicycle from '~icons/mdi/bicycle';
+	// import IconBicycleCargo from '~icons/mdi/bicycle-cargo';
+	// import IconBicycleBasket from '~icons/mdi/bicycle-basket';
+</script>
+
+<div class="w-full bg-surface-100-800-token py-4">
+	<div class="px-8 flex justify-center items-center">
+		<div class="w-0 grow flex justify-end">
+			<IconBike width={80} height={80}></IconBike>
+		</div>
+
+			<h2 class="ml-8 h2 grow"> Cycling Advocates of Cuyahoga Falls</h2>		
+	</div>
+
+	<!-- <div class="flex justify-center items-center px-8 max-md:gap-x-12 gap-x-20">
+			<IconBicycleBasket width={60} height={60}></IconBicycleBasket>
+			<IconBicycle width={60} height={60}></IconBicycle>
+			<IconBicycleCargo width={60} height={60}></IconBicycleCargo>
+	</div> -->
+
+</div>
+
+<div class="container mx-auto py-8 px-4">
+	<slot />
+</div>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+
+</script>
+
+<div class="card px-6 pb-6 pt-2 mb-6">Basic</div>
